6f41e475f1ec8907c4dcdec8281167b77e4857ab
fzentenosmith <zenteno.fco@gmail.com>
2025-06-01 18:09:37 -0500

    updated requirements

--- src/business/problem.py
+++ src/business/problem.py
@@ -4,12 +4,12 @@ import numpy as np
 class Problem:
     def __init__(
         self,
-        warehouses: int,
-        customers: int,
-        fixed_cost: np.ndarray,
-        shipping_cost: np.ndarray,
-        demand: np.ndarray,
-        capacity: np.ndarray,
+        warehouses: np.ndarray,  # (m,2) locations
+        customers: np.ndarray,  # (n,2) location
+        fixed_cost: np.ndarray,  # (m,)
+        shipping_cost: np.ndarray,  # (m,n)
+        demand: np.ndarray,  # (n,)
+        capacity: np.ndarray,  # (m,)
     ):
         self.warehouses = warehouses
         self.customers = customers
--- src/main.py
+++ src/main.py
@@ -1,5 +1,35 @@
+import numpy as np
+
+from business.problem import Problem
+
+
 def main():
     print("Starting...")
+    build_problem_1()
+
+
+def build_problem_1():
+    warehouses = np.array([[0, 0], [2, 2], [4, 4]])
+    customers = np.array([[1, 0], [0, 1], [1, 3], [3, 1], [5, 2], [2, 5]])
+    fixed_costs = np.array([1, 1, 1])
+    shipping_cost = np.zeros((len(warehouses), len(customers)))
+    for i in range(len(warehouses)):
+        wi = warehouses[i]
+        for j in range(len(customers)):
+            cj = customers[j]
+            shipping_cost[i, j] = np.round(np.sqrt(np.sum((wi - cj) ** 2)), decimals=1)
+    demand = np.arange(1, len(customers) + 1)
+    capacity = np.ones(len(warehouses)) * ((np.sum(demand) / len(warehouses)) + 1)
+
+    # Create the problem instance
+    return Problem(
+        warehouses=warehouses,
+        customers=customers,
+        fixed_cost=fixed_costs,
+        shipping_cost=shipping_cost,
+        demand=demand,
+        capacity=capacity,
+    )
 
 
 if __name__ == "__main__":
